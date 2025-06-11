db1869689ecca3416910c22ead882cc367c40d2a
MadV313 <snowvonia@gmail.com>
2025-06-10 23:13:35 -0400

    Update inventory.py

--- utils/inventory.py
+++ utils/inventory.py
@@ -1,1 +1,16 @@
 
+# utils/inventory.py
+
+from collections import Counter
+
+def has_required_parts(inventory, requirements):
+    inv_count = Counter(inventory)
+    for item, qty in requirements.items():
+        if inv_count[item] < qty:
+            return False
+    return True
+
+def remove_parts(inventory, requirements):
+    for item, qty in requirements.items():
+        for _ in range(qty):
+            inventory.remove(item)
